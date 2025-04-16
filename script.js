ee3703975a2809d46f3fe44a28524da3e300e452
Mustafa Bin Aamir <aamirmustafabin@gmail.com>
2025-04-16 13:31:28 +0500

    new fix

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function addTask() {
-  
-  }     
+function addTodo() {
+  const input = document.getElementById("todo-input");
+  const task = input.value;
+  if (task === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  li.onclick = () => li.remove();
+
+  document.getElementById("todo-list").appendChild(li);
+  input.value = ""; 
+}
+
